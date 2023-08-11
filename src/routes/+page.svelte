8bc9bcdef7ea7f007436e38e07c40197e263a4b2
ihsanvp <ihsanvp2738@gmail.com>
2023-08-11 16:16:22 +0530

    add: basic ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <meta
+        name="description"
+        content="Unlock Actionable Insights with Our Business Analytics App - Transform your data into strategic decisions with our powerful business analytics app. Visualize trends, track key metrics, and make informed choices that drive growth. Experience real-time data analysis at your fingertips. Try our business analytics app today for sharper insights and smarter business moves."
+    />
+    <title>Business App | Studiotwofour</title>
+</svelte:head>
+<div
+    class="w-full min-h-screen flex items-center justify-center flex-col gap-10"
+>
+    <p class="text-primary">Business Application</p>
+    <h1 class="text-6xl font-bold text-center max-w-4xl">
+        Empower Your Business with Advanced Analytics
+    </h1>
+    <p class="max-w-md text-center opacity-60">
+        A comprehensive dashboard allowing you to manage all aspects of your
+        business
+    </p>
+    <div class="mt-20">
+        <button
+            class="px-20 py-4 bg-primary hover:bg-lime-500 text-black font-medium rounded-md"
+        >
+            Download now
+        </button>
+    </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#adfa1d"
+      }
+    },
   },
 
   plugins: [],

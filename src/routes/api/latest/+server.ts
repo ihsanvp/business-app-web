a44b042e44043e5cad8539058538aed76e19679d
ihsanvp <ihsanvp2738@gmail.com>
2023-08-13 17:25:46 +0530

    debug

--- src/routes/api/latest/+server.ts
+++ src/routes/api/latest/+server.ts
@@ -9,23 +9,27 @@ const schema = z.object({
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ platform, request }) {
-    const body = await request.json()
-    const validated = schema.safeParse(body)
+    try {
+        const body = await request.json()
+        const validated = schema.safeParse(body)
 
-    if (!validated.success) {
-        throw error(400, `Validation: ${validated.error.message}`)
+        if (!validated.success) {
+            throw error(400, `Validation: ${validated.error.message}`)
+        }
+
+        const identifier = getDeviceIdentifier(validated.data.device)
+        const release = await getLatestRelease()
+        const asset = release.assets.find(asset => asset.name.endsWith(identifier as string))
+
+        if (!asset) {
+            throw error(400, `Asset not found for device: ${validated.data.device}`)
+        }
+
+        return json({
+            url: asset.browser_download_url,
+            name: asset.name
+        })
+    } catch (err) {
+        throw error(400, String(err))
     }
-
-    const identifier = getDeviceIdentifier(validated.data.device)
-    const release = await getLatestRelease()
-    const asset = release.assets.find(asset => asset.name.endsWith(identifier as string))
-
-    if (!asset) {
-        throw error(400, `Asset not found for device: ${validated.data.device}`)
-    }
-
-    return json({
-        url: asset.browser_download_url,
-        name: asset.name
-    })
 }
